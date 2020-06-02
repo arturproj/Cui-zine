3fab08e90c4a6618e21e3582e48ce7e9d927d0dc
Artur DevOps <50926041+arturproj@users.noreply.github.com>
2020-06-02 11:13:55 +0200

    update assets

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,7 @@
 $(function () {
-    console.log('document ready')
+    console.log('document ready');
+    $(document).scroll(function () {
+        var $nav = $(".navbar");
+        $nav.toggleClass('bg-transparent', $(this).scrollTop() < $(".navbar").height());
+    });
 });
